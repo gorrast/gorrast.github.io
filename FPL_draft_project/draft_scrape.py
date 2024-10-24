d76013395ca55df8fbfe3705ca1ef78e6a2eb98b
gorast <hugo@wigh.nu>
2024-10-24 13:43:14 +0200

    Fixed path handling in script

--- FPL_draft_project/draft_scrape.py
+++ FPL_draft_project/draft_scrape.py
@@ -2,6 +2,7 @@ import requests
 import json
 import logging
 from logging.handlers import RotatingFileHandler
+import os
 
 def api_request(league_id):
     url = f"https://draft.premierleague.com/api/league/{league_id}/details"
@@ -15,7 +16,7 @@ def api_request(league_id):
         quit()
 
         
-def update_data(data, entries):
+def update_data(data, entries, script_dir):
     response = api_request(LEAGUE_ID)
     gameweek = int(response['standings'][0]['matches_drawn']) + int(response['standings'][0]['matches_lost']) + int(response['standings'][0]['matches_won'])
 
@@ -44,27 +45,30 @@ def update_data(data, entries):
         data[name][gameweek]['gw_score_for'] = gw_score_for
         data[name][gameweek]['gw_score_against'] = gw_score_against
     
-    
     # Save the data to file
-    save_as_json(data)
-    
+    save_as_json(data, script_dir)
     
     logging.info('Data updated successfully!')
     
     return data
 
-def save_as_json(data):
-    with open('draft_data.json', 'w') as file:
+def save_as_json(data, file_path):
+
+    with open(os.path.join(file_path, 'draft_data.json'), 'w') as file:
         json.dump(data, file)
         
         
     logging.info('Data saved as JSON-file')
 
-def read_json():
-    with open('draft_data.json', 'r') as file:
+def read_json(file_path):
+
+    data_path = os.path.join(file_path, 'draft_data.json')
+    entries_path = os.path.join(file_path, 'draft_entries.json')
+
+    with open(data_path, 'r') as file:
         data = json.load(file)
         
-    with open('draft_entries.json', 'r') as file:
+    with open(entries_path, 'r') as file:
         entries = json.load(file)
         
     return data, entries
@@ -86,16 +90,25 @@ def main():
     global LEAGUE_ID
     LEAGUE_ID = '17526'
     configure_logging()
-    data, entries = read_json()
+
+    # Get the directory of the current script
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    print(script_dir)
+
+    """ # Define the path to the .json file relative to the script's location
+    file_path = os.path.join(script_dir, 'FPL_draft_project') """
+
+    data, entries = read_json(script_dir)
     
     for gw in range(len(data['Gorast Gunners'])):
         if data['Gorast Gunners'][gw] == {}:
             global max_gw
             max_gw = gw - 1
             break
-    data = update_data(data, entries)
 
-                
+    data = update_data(data, entries, script_dir)
+
+    
 
 
         
